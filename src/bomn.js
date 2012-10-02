387e94483399f0a2bb8f0eac0f1b0e52c1c53fa4
ktf <not.discostoo@gmail.com>
2012-10-01 18:17:03 -0700

    animate explosions

--- src/bomn.js
+++ src/bomn.js
@@ -1,6 +1,8 @@
 (function(b, undefined) {
   'use strict';
 
+  var EXPLOSION_LENGTH = 1000; // in milliseconds
+
   /*
    * The Bomn object -- it'll blow you up!
    */
@@ -12,6 +14,7 @@
     this.image = {};
     this.exploding = false;
     this.exploded = false;
+    this.explosionTimer = 0; // used to control explosion animation
   };
 
 
@@ -32,14 +35,18 @@
 
 
   /*
-   * LOOK OUT! Wipes out the area in the blast radius. Takes the level's tiles.
+   * LOOK OUT! Wipes out the area in the blast radius. Takes the level's tiles
+   * as a parameter.
    */
   b.Bomn.prototype.explode = function(tiles) {
     this.exploding = true;
-    // clear the blast radius
+    this.explosionTimer = Date.now();
+
+    // calculate the tiles in the blast radius
     var explosionSize = (2 * this.blastRadius) + 1;
     var startRow = this.row - this.blastRadius;
     var startCol = this.col - this.blastRadius;
+
     for(var row = startRow; row < startRow + explosionSize; row++) {
       for(var col = startCol; col < startCol + explosionSize; col++) {
         // check if it's onscreen, then clear
@@ -68,31 +75,43 @@
 
 
   /*
-   * Draw the bomn to the given context.
+   * Draw the bomn to the given context. Or if the bomn is exploding, draw the
+   * explosion.
    */
   b.Bomn.prototype.draw = function(context) {
-    // draw the bomn, or if it's exploding, draw the explosion
     if(this.exploding) {
+      // convert our explosion "radius" into the square that it actually is
       var explosionSize = (2 * this.blastRadius) + 1;
       var startRow = this.row - this.blastRadius;
       var startCol = this.col - this.blastRadius;
+
       for(var row = startRow; row < startRow + explosionSize; row++) {
         for(var col = startCol; col < startCol + explosionSize; col++) {
+          // adjust row,col values to pixel values
           var pixelX = col * b.TILE_SIZE;
           var pixelY = row * b.TILE_SIZE;
-          context.fillStyle = 'orange';
+
+          // The alpha value for the explosion adjusts as time passes to fade
+          // the explosion away. Over the course of EXPLOSION_LENGTH, the alpha
+          // needs to go from 1 (opaque) to 0 (transparent).
+          var elapsed = Date.now() - this.explosionTimer;
+          var alphaValue = (1 - (elapsed / EXPLOSION_LENGTH));
+          if(alphaValue > 1) { alphaValue = 1; }
+          if(alphaValue < 0) { alphaValue = 0; }
+
+          context.fillStyle = 'rgba(255, 165, 0, ' + alphaValue + ')';
           context.fillRect(pixelX, pixelY, b.TILE_SIZE, b.TILE_SIZE);
         }
       }
 
       // once we've drawn the explosion, this bomn is spent and can die...
-      // after the explosion hangs out for another second
-      if(Date.now() - this.timer >= (b.BOMN_TIMER * 1000) + 1000) {
+      // after the explosion hangs out on-screen for EXPLOSION_LENGTH ms
+      if(Date.now() - this.timer >= (b.BOMN_TIMER * 1000) + EXPLOSION_LENGTH) {
         this.exploded = true;
       }
     }
 
-    // not exploding, just draw normally
+    // not exploding, draw the ticking timer
     else {
       var elapsedTime = Date.now() - this.timer;
       var display = 0;
