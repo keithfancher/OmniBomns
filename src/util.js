d5cd78363caea90c905fd959e1614cfc25254990
ktf <not.discostoo@gmail.com>
2012-10-03 12:56:40 -0700

    remove unused util functions

--- src/util.js
+++ src/util.js
@@ -2,29 +2,10 @@
   'use strict';
 
   /*
-   * Draw text with given specs to given context.
-   */
-  b.drawText = function(context, text, x, y, size, color) {
-    context.font = size + ' courier';
-    context.fillStyle = color;
-    context.fillText(text, x, y);
-  };
-
-
-  /*
    * Return random integer between min and max (inclusive!).
    */
   b.randomInt = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
-
-  /*
-   * Round given value to nearest tenth... necessary since adding and
-   * subtracting 0.1 seems to be occasionally imprecise.
-   */
-  b.roundToTenth = function(num) {
-    return Math.round(num * 10) / 10;
   };
 
 })(window.bomns = window.bomns || {});
